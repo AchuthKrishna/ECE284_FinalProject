6aef7da33a6ab13035727201ca3a74095e6ead09
mjarecky <mattjarecky@yahoo.com>
2025-12-03 06:52:32 -0800

    alpha 4; openroad

---
+++ hardware/openroad_syn_pnr/part1/results/ihp-sg13g2/core/base/2_2_floorplan_macro.tcl
@@ -0,0 +1,7 @@
+place_macro -macro_name pmem_instance/gen_2048x64\[0\].i_cut -location {56.8 1889.87} -orientation R0
+place_macro -macro_name pmem_instance/gen_2048x64\[1\].i_cut -location {921.28 1889.87} -orientation MY
+place_macro -macro_name xmem_instance/gen_512x32\[0\].i_cut -location {2099.36 59.09} -orientation R0
+place_macro -macro_name xmem_instance/gen_512x32\[1\].i_cut -location {609.44 59.09} -orientation MY
+place_macro -macro_name xmem_instance/gen_512x32\[2\].i_cut -location {1602.72 59.09} -orientation R0
+place_macro -macro_name xmem_instance/gen_512x32\[3\].i_cut -location {1106.08 59.09} -orientation MY
+
